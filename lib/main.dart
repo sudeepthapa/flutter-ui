b32a42745630f4bf2dfb4c267283f154e72944ba
sudeptech <sudeptech@gmail.com>
2019-03-22 16:03:55 +0545

    Flutter card profile ui

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:profile_card/pages/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Card Demo",
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:profile_card/pages/profile_card.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text("Flutter UIs"),
+        actions: <Widget>[Container(
+          padding: EdgeInsets.all(15),
+          child: Icon(Icons.favorite))],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            AppBar(
+              title: Text('Flutter UI'),
+              backgroundColor: Colors.deepOrange,
+            ),
+            ListTile(
+              leading: Icon(Icons.arrow_right, color: Colors.deepOrangeAccent,),
+                title: Text("Card Profile", style: TextStyle(color:Colors.black87, fontSize: 18),),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => ProfileCard()));
+                })
+          ],
+        ),
+      ),
+      body: Center(
+        child: Container(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Navigate to View All UIs.",style: TextStyle(fontSize: 20, color: Colors.deepPurple),),
+              Builder(
+                 builder:(BuildContext context)=>RaisedButton(
+                  child: Icon(Icons.menu, color: Colors.white,),
+                  color: Colors.deepOrange,
+                  onPressed: (){
+                    Scaffold.of(context).openDrawer();
+                  },
+                ),
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
