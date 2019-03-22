ec296e468db8820308a2fe4dcccf4b96f462c8a6
sudeptech <sudeptech@gmail.com>
2019-03-22 16:23:02 +0545

    status bar color changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:profile_card/pages/home_page.dart';
 
 void main() {
+ SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  statusBarColor: Colors.deepOrangeAccent
+));
   runApp(
-    MaterialApp(
+     MaterialApp(
       title: "Card Demo",
       home: HomePage(),
       debugShowCheckedModeBanner: false,
