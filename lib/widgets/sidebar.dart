1d3d5c2e83eba1840440b0697e4c0684d2764396
sudeptech <sudeptech@gmail.com>
2019-04-01 13:42:37 +0545

    placeList 1 completed

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:profile_card/pages/lists/directory_concept/placelist1.dart';
 import 'package:profile_card/pages/login_pages/login_form.dart';
 import 'package:profile_card/pages/login_pages/login_page2.dart';
 import 'package:profile_card/pages/missaleneous/home_landing.dart';
@@ -11,9 +12,9 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.deepOrange,
         title: Text("Flutter UIs"),
-        actions: <Widget>[Container(
-          padding: EdgeInsets.all(15),
-          child: Icon(Icons.favorite))],
+        actions: <Widget>[
+          Container(padding: EdgeInsets.all(15), child: Icon(Icons.favorite))
+        ],
       ),
       drawer: Drawer(
         child: ListView(
@@ -25,86 +26,155 @@ class HomePage extends StatelessWidget {
             ),
             ExpansionTile(
               leading: Icon(Icons.lock),
-              title: Text("Login UI", style: TextStyle(color: Colors.black, fontFamily: 'Roboto', fontSize: 18 ),),
+              title: Text(
+                "Login UI",
+                style: TextStyle(
+                    color: Colors.black, fontFamily: 'Roboto', fontSize: 18),
+              ),
               children: <Widget>[
                 ListTile(
-              leading: Icon(Icons.arrow_right, color: Colors.deepOrangeAccent,),
-                title: Text("Login Form", style: TextStyle(color:Colors.black87, fontSize: 14),),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => LoginForm()));
-                }
-              ),
-            ListTile(
-              leading: Icon(Icons.arrow_right, color: Colors.deepOrangeAccent,),
-                title: Text("Login Page2", style: TextStyle(color:Colors.black87, fontSize: 14),),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => LoginPage2()));
-                }
-              )
+                    leading: Icon(
+                      Icons.arrow_right,
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    title: Text(
+                      "Login Form",
+                      style: TextStyle(color: Colors.black87, fontSize: 14),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => LoginForm()));
+                    }),
+                ListTile(
+                    leading: Icon(
+                      Icons.arrow_right,
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    title: Text(
+                      "Login Page2",
+                      style: TextStyle(color: Colors.black87, fontSize: 14),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => LoginPage2()));
+                    })
               ],
             ),
             ExpansionTile(
-              title: Text("Profile UI", style: TextStyle(color: Colors.black, fontFamily: 'Roboto', fontSize: 18 ),),
+              title: Text(
+                "Profile UI",
+                style: TextStyle(
+                    color: Colors.black, fontFamily: 'Roboto', fontSize: 18),
+              ),
               leading: Icon(Icons.person),
               children: <Widget>[
                 ListTile(
-              leading: Icon(Icons.arrow_right, color: Colors.deepOrangeAccent,),
-                title: Text("Card Profile 1", style: TextStyle(color:Colors.black87, fontSize: 14),),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => ProfileCard()));
-                }
-              ),
+                    leading: Icon(
+                      Icons.arrow_right,
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    title: Text(
+                      "Card Profile 1",
+                      style: TextStyle(color: Colors.black87, fontSize: 14),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  ProfileCard()));
+                    }),
               ],
             ),
             ExpansionTile(
-              title: Text("Misallenaous", style: TextStyle(color: Colors.black, fontFamily: 'Roboto', fontSize: 18 ),),
+              title: Text(
+                "Misallenaous",
+                style: TextStyle(
+                    color: Colors.black, fontFamily: 'Roboto', fontSize: 18),
+              ),
               leading: Icon(Icons.loupe),
               children: <Widget>[
                 ListTile(
-              leading: Icon(Icons.arrow_right, color: Colors.deepOrangeAccent,),
-                title: Text("Home Landing", style: TextStyle(color:Colors.black87, fontSize: 14),),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => HomeLanding()));
-                }
-              ),
+                    leading: Icon(
+                      Icons.arrow_right,
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    title: Text(
+                      "Home Landing",
+                      style: TextStyle(color: Colors.black87, fontSize: 14),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  HomeLanding()));
+                    }),
               ],
             ),
-            
-            
-            
+            ExpansionTile(
+              title: Text(
+                "List Ui",
+                style: TextStyle(
+                    color: Colors.black, fontFamily: 'Roboto', fontSize: 18),
+              ),
+              leading: Icon(Icons.list),
+              children: <Widget>[
+                 ExpansionTile(
+                   backgroundColor: Colors.black12,
+                   title: Text('Directory Concept'),
+                   leading: Icon(Icons.looks),
+                   children: <Widget>[
+                     ListTile(
+                    leading: Icon(
+                      Icons.arrow_right,
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    title: Text(
+                      "Place List 1",
+                      style: TextStyle(color: Colors.black87, fontSize: 14),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  PlaceList1()));
+                    })
+                   ],
+                 )
+              ],
+            )
           ],
         ),
       ),
       body: Center(
         child: Container(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Navigate to View All UIs.",style: TextStyle(fontSize: 20, color: Colors.deepPurple),),
-              Builder(
-                 builder:(BuildContext context)=>RaisedButton(
-                  child: Icon(Icons.menu, color: Colors.white,),
-                  color: Colors.deepOrange,
-                  onPressed: (){
-                    Scaffold.of(context).openDrawer();
-                  },
-                ),
-              )
-            ],
-          )
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Navigate to View All UIs.",
+              style: TextStyle(fontSize: 20, color: Colors.deepPurple),
+            ),
+            Builder(
+              builder: (BuildContext context) => RaisedButton(
+                    child: Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                    ),
+                    color: Colors.deepOrange,
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                  ),
+            )
+          ],
+        )),
       ),
     );
   }
